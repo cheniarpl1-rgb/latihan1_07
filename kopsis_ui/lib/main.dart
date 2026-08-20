0301042dbe1a1bd2d651224b2899217a01938497
Chenia <chenia.rpl1@gmail.com>
2026-08-20 13:24:21 +0700

    LKM-2 tata letak

--- kopsis_ui/lib/main.dart
+++ kopsis_ui/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -8,9 +10,28 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(title: const Text('Koperasi Sekolah')),
-        body: const Center(child: Text('Kasir Koperasi')),
+        appBar: AppBar(
+          title: const Text('Kopsis UI'),
+        ),
+        body: Container(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text('Buku Tulis'),
+              const SizedBox(height: 8),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Text('Anggota: Rp3.000'),
+                  Text('Umum: Rp3.500'),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
